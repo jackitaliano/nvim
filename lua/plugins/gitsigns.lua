b3815599fa0809770f4f63d94c89a7c5faee5528
jackitaliano <italiano.16@buckeyemail.osu.edu>
2023-09-04 14:57:45 -0400

    change keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -30,19 +30,20 @@ return {
 			map('n', '[h', gs.prev_hunk, { buffer = bufnr, desc = 'Previous Hunk' })
 			map('n', ']h', gs.next_hunk, { buffer = bufnr, desc = 'Next Hunk' })
 			map('n', '<leader>gb', gs.toggle_current_line_blame, { desc = 'Blame Line' })
+			map('n', '<leader>gs', gs.stage_buffer, { desc = 'Stage Buffer' })
+			map('n', '<leader>gr', gs.reset_buffer, { desc = 'Reset Buffer' })
+			map('n', '<leader>gd', function() gs.diffthis('~') end, { desc = 'Diff Buffer' })
+
 			map('n', '<leader>hs', gs.stage_hunk, { desc = 'Stage Hunk' })
 			map('n', '<leader>hr', gs.reset_hunk, { desc = 'Reset Hunk' })
-			map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Stage Hunk' })
-			map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, { desc = 'Reset Hunk' })
+			map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = 'Stage Hunk' })
+			map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, { desc = 'Reset Hunk' })
 
-			map('n', '<leader>hS', gs.stage_buffer, { desc = 'Stage Buffer' })
 			map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'Undo Stage' })
-			map('n', '<leader>hR', gs.reset_buffer, { desc = 'Reset Buffer' })
-			map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview Hunk'})
-			map('n', '<leader>hb', function() gs.blame_line{full=true} end, { desc = 'Blame Hunk' })
+			map('n', '<leader>hp', gs.preview_hunk, { desc = 'Preview Hunk' })
+			map('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = 'Blame Hunk' })
 			map('n', '<leader>hd', gs.diffthis, { desc = 'Diff Hunk' })
-			map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = 'Diff Buffer' })
-		    -- map('n', '<leader>td', gs.toggle_deleted)
+			-- map('n', '<leader>td', gs.toggle_deleted)
 		end,
 	},
 }
